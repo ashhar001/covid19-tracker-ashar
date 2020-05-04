ec7a82199751f8356d9257d3bd37f5700a33ea06
ashar <ashar.ansari1020@gmail.com>
2020-05-04 11:12:43 +0530

    Updates

---
+++ precache-manifest.9eb1515ecbed1c564caf7a18ef62aa8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5cd6a2c52bf40ffd95a6df158f140f",
+    "url": "/covid19-tracker-ashar/index.html"
+  },
+  {
+    "revision": "e77924c609c9b34dabfb",
+    "url": "/covid19-tracker-ashar/static/css/main.b8c7229c.chunk.css"
+  },
+  {
+    "revision": "700e2598ae226e9d3b79",
+    "url": "/covid19-tracker-ashar/static/js/2.c773e98c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker-ashar/static/js/2.c773e98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e77924c609c9b34dabfb",
+    "url": "/covid19-tracker-ashar/static/js/main.b7b86ac6.chunk.js"
+  },
+  {
+    "revision": "752d973f350b65831c1d",
+    "url": "/covid19-tracker-ashar/static/js/runtime-main.cf48049a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker-ashar/static/media/image.d7265326.png"
+  }
+]);
